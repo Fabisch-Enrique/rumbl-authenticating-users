dea54a27baec245ea6b7c97dc898af2fd6cfa2b9
Fabisch <drenriquenicol@gmail.com>
2022-09-30 12:16:12 +0300

    a commit

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,7 +2,7 @@ alias Rumbl.Repo
 alias Rumbl.Media.Category
 
 for category <- ~w(Action Drama Romance Comedy Sci Fi) do
-  Repo.get_by(Category, name: category) ||
+  Repo.get_by!(Category, name: category) ||
   Repo.insert!(%Category{name: category})
 end
 
